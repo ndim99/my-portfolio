e8109c0eb51c5e123917cc4f9b754c6b00c48afb
blueZircon123 <146864939+ndim99@users.noreply.github.com>
2024-12-06 10:58:44 +0100

    feat: update "ClientSideDataFetching" component

--- src/components/DataFetch/ClientSideDataFetching.tsx
+++ src/components/DataFetch/ClientSideDataFetching.tsx
@@ -3,6 +3,7 @@ import { TokenData } from "@/types/tokenData";
 import { useState, useEffect } from "react";
 import { formatNumericAmountCompact } from "@/utils/formatting";
 import { formatPrice } from "@/utils/priceFormatting";
+import LoadingIcon from "../Icons/LoadingIcon";
 
 interface ClientSideDataFetchingProps {
   tokenId: string;
@@ -68,17 +69,17 @@ export function ClientSideDataFetching({
   }, [tokenId]); // Dependency ensures re-fetching if tokenId changes
 
   return (
-    <div className="w-full 2xl:p-4 p-3 bg-gray-100 dark:bg-gray-900 rounded-lg flex flex-col">
+    <div className="w-full 2xl:p-4 p-3 bg-gray-100 dark:bg-gray-900 rounded-lg flex flex-col justify-center">
       {loading ? (
-        <div className="flex items-center justify-center fontSizeFromXl font-semibold text-primary-colors">
-          Loading...
+        <div className="flex items-center justify-center space-x-2 fontSizeFromXl font-semibold text-primary-colors">
+          <LoadingIcon />
         </div>
       ) : error ? (
         <div className="flex items-center justify-center fontSizeFromXl font-semibold text-red-500">
           Error: {error}
         </div>
       ) : (
-        <div className="flex flex-col 2xl:gap-4 gap-3">
+        <div className="flex flex-col justify-between 2xl:gap-4 gap-3">
           <div className="flex items-center gap-2">
             <Image
               src={`/${data?.name}.png`}
@@ -116,12 +117,15 @@ import { TokenData } from "@/types/tokenData";
 import { useState, useEffect } from "react";
 import { formatNumericAmountCompact } from "@/utils/formatting";
 import { formatPrice } from "@/utils/priceFormatting";
+import LoadingIcon from "../Icons/LoadingIcon";
 
-interface ClientSideDataProps {
+interface ClientSideDataFetchingProps {
   tokenId: string;
 }
 
-export default function ClientSideData({ tokenId }: ClientSideDataProps) {
+export function ClientSideDataFetching({
+  tokenId,
+}: ClientSideDataFetchingProps) {
   const [data, setData] = useState<TokenData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -131,7 +135,7 @@ export default function ClientSideData({ tokenId }: ClientSideDataProps) {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          https://api.coingecko.com/api/v3/coins/{tokenId}
+          https://api.coingecko.com/api/v3/coins/tokenId
         );
         if (!response.ok) throw new Error("Failed to fetch data");
 
@@ -155,50 +159,21 @@ export default function ClientSideData({ tokenId }: ClientSideDataProps) {
     fetchData();
   }, [tokenId]); // Dependency ensures re-fetching if tokenId changes
 
-  // Reusable Row component for cleaner structure
-  const DataRow = ({
-    label,
-    value,
-  }: {
-    label: string;
-    value?: string | number;
-  }) => {
-    const isPriceChange = label === "Price Change (24h)";
-    const isNegative = typeof value === "string" && value.includes("-");
-
-    return (
-      <div className="flex justify-between items-center fontSizeFromLg text-primary-colors">
-        <span className="font-normal">{label}</span>
-        <span
-          className={font-semibold {
-            isPriceChange
-              ? isNegative
-                ? "text-red-500"
-                : "text-green-500"
-              : ""
-          }}
-        >
-          {value || "N/A"}
-        </span>
-      </div>
-    );
-  };
-
   return (
-    <div className="w-full 2xl:p-4 p-3 bg-gray-100 dark:bg-gray-900 rounded-lg flex flex-col">
+    <div className="w-full 2xl:p-4 p-3 bg-gray-100 dark:bg-gray-900 rounded-lg flex flex-col justify-center">
       {loading ? (
-        <div className="flex items-center justify-center fontSizeFromXl font-semibold text-primary-colors">
-          Loading...
+        <div className="flex items-center justify-center space-x-2 fontSizeFromXl font-semibold text-primary-colors">
+          <LoadingIcon />
         </div>
       ) : error ? (
         <div className="flex items-center justify-center fontSizeFromXl font-semibold text-red-500">
           Error: {error}
         </div>
       ) : (
-        <div className="flex flex-col 2xl:gap-4 gap-3">
+        <div className="flex flex-col justify-between 2xl:gap-4 gap-3">
           <div className="flex items-center gap-2">
             <Image
-              src={/{data?.name}.png}
+              src={data?.name}.png}
               alt={data?.symbol as string}
               className="rounded-full 2xl:w-8 lg:w-7 w-7 2xl:h-8 lg:h-7 h-6"
               width={32}
